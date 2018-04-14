7ffb87ee486ea4dcfa21ae09f00c2b52a1c68862
blue885 <tandung85@gmail.com>
2018-04-14 13:53:59 +0700

    Sửa ngôn ngữ

--- MVCDTO/CommonTasks/ListChargeRateDTO.cs
+++ MVCDTO/CommonTasks/ListChargeRateDTO.cs
@@ -1,6 +1,7 @@
 ﻿using MVCModel;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,17 +14,25 @@ namespace MVCDTO.CommonTasks
         public void SetID(int id) { this.ChargeRateID = id; }
 
         public int ChargeRateID { get; set; }
-   
+
+        [Display(Name = "Loại phòng")]
         public int RoomTypeID { get; set; }
+        [Display(Name = "Loại phòng")]
         public string ListRoomTypeDescription { get; set; }
+        [Display(Name = "Đơn vị tính")]
         public int ChargeTypeID { get; set; }
+        [Display(Name = "Đơn vị tính")]
         public string ListChargeTypeDescription { get; set; }
         public int ChargeVolumn { get; set; }
+        [Display(Name = "Đơn giá")]
         public double ChargeRate { get; set; }
+        [Display(Name = "Đơn giá phụ thu")]
         public double ChargeRateUpper { get; set; }
+        [Display(Name = "Diễn giải")]
         public string Description { get; set; }
+        [Display(Name = "Ghi chú")]
         public string Remarks { get; set; }
-            
+
     }
 
     public class ListChargeRateDTO : ListChargeRatePrimitiveDTO
